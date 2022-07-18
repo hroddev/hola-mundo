a84dfd3059ca7273662792d65105bbb18f23f7a3
Hector Rodriguez <hector@hrod.dev>
2022-07-17 19:37:24 -0500

    add components greeting with class and functions

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Greeting from './components/pure/greeting';
+import GreetingF from './components/pure/greetingF';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        {/* <Greeting name="Héctor"/> */}
+        <GreetingF name="Héctor"/>
         <a
           className="App-link"
           href="https://reactjs.org"
